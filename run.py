5babad413325fe8653a5fa6ec752ebeebc934b88
rodriguesprobr <fernando@rodrigues.pro.br>
2018-05-03 10:25:23 -0300

    Coding adjustments.

--- run.py
+++ run.py
@@ -14,6 +14,6 @@
 import importlib, image_module as im
 
 # Insert on the variable below your Google Photo full path. For example: /home/user/your@email/Google Photos
-path = "/home/orionx/googledrive/Google Fotos"
+path = "/home/user/your@email/Google Photos"
 
 im.find_duplicates(path)
