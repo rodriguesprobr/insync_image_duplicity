616aaf451124c6c4e7fa86312b127557ca9b230a
rodriguesprobr <fernando@rodrigues.pro.br>
2018-05-03 10:24:32 -0300

    Coding adjustments.

--- image_module.py
+++ image_module.py
@@ -3,39 +3,36 @@
 
 ###
 #title           :image_module.py
-#description     :Image duplicate module with the methods.
+#description     :Image duplicate module.
 #author          :Fernando de Assis Rodrigues
-#date            :20170411
+#date            :20180503
 #usage           :import image_module
 #notes           :https://github.com/rodriguesprobr/insync_image_duplicity
-#python_version  :3.7
+#python_version  :3.5.2
 ###
 
-import os, filecmp
-def list(type, path):
-	list = os.scandir(path)
-	files = []
-	for entry in list:
-		if entry.is_dir():
-			list_duplicities(str(path+"/"+entry.name))
-		if entry.is_file():
-			files.append(entry)
-	files = sorted(files, key=lambda e: e.name, reverse=True)
-	#files = sorted(files, reverse=True)
-	#compararfiles(files, path)
-	#print(files)
-	if type == "stdout":
-		for i in files:
-			print(i.name)
-	elif type == "return": 
-		return files
+import os, filecmp, image_module as im
 
-def rm(type, path):
-	files = list("return", path)
-	for file_a in files:
-		for file_b in files:
-			if file_a != file_b:
-				if filecmp.cmp(str(path + "/" + file_a.name), str(path + "/" + file_b.name)):
-					if type == "stdout":
-						print("rm -f  " + path.replace(" ", "\\ ").replace("(", "\\(").replace(")", "\\)") + "/" + file_b.name.replace(" ", "\\ ").replace("(", "\\(").replace(")", "\\)"))
-					files.remove(file_b)
+def find_duplicates(path):
+    list = os.scandir(path)
+    files = []
+    dirs = []
+    for entry in list:
+        if entry.is_dir():
+            dirs.append(entry)
+        elif entry.is_file():
+            files.append(entry)
+    compare(files, path)
+    for d in dirs:
+        find_duplicates(path + "/" + d.name)
+
+def compare(files, path):
+    for file_a in files:
+        for file_b in files:
+            if file_a != file_b:
+                if filecmp.cmp(str(path + "/" + file_a.name), str(path + "/" + file_b.name)):
+                    print("rm -f  " + \
+                        path.replace(" ", "\ ").replace("(", "\(").replace(")", "\)") + \
+                        "/" + \
+                        file_b.name.replace(" ", "\ ").replace("(", "\(").replace(")", "\)"))
+                    files.remove(file_b)
--- run.py
+++ run.py
@@ -3,24 +3,17 @@
 
 ###
 #title           :run.py
-#description     :Run this script remove image duplicities found in path variable.
+#description     :Run this script find duplicates images on Google Photos.
 #author          :Fernando de Assis Rodrigues
-#date            :20170411
+#date            :20180503
 #usage           :import image_module
 #notes           :https://github.com/rodriguesprobr/insync_image_duplicity
-#python_version  :3.7
+#python_version  :3.5.2
 ###
 
 import importlib, image_module as im
 
-# Insert your Google Photo folder, with full path. Example: /home/user/your@email/Google Photos
-path = "/home/orionx/Downloads/11"
+# Insert on the variable below your Google Photo full path. For example: /home/user/your@email/Google Photos
+path = "/home/orionx/googledrive/Google Fotos"
 
-# List command that retrieves the files that will be analyzed.
-# Made for debugging purposes and it's not required to be enabled.
-# im.list("stdout", path)
-
-# Enable the command below to run the safe option.
-# The rm command will make a stdout string, with a bunch of rm commands to remove all duplicates, manually.
-# To run, I suggest something like python3 run.py >> to a output.text, with this command enabled.
-im.rm("stdout", path)
+im.find_duplicates(path)
